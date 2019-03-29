b56ae23472068eafc2c4dea2cc81b6c0c54a48aa
“Kimberly <kimberly.achkio@gmail.com>
2019-03-29 11:08:44 -0600

    Update Photo.js

---
+++ Photo.js
@@ -0,0 +1,9 @@
+class Photo {
+  constructor(title, caption, file, id) {
+    this.title = title
+    this.caption = caption
+    this.file = file
+    this.id = id
+    this.favorite = false
+  }
+}
