4e3d10bbfe0cc88b4c5760d223a33791f2b189a8
“Kimberly <kimberly.achkio@gmail.com>
2019-03-31 19:46:18 -0600

    Favor and delete photos

--- Photo.js
+++ Photo.js
@@ -6,4 +6,13 @@ class Photo {
     this.id = id
     this.favorite = false
   }
+
+  saveToStorage() {
+    localStorage.setItem('imagesLocalStorage', JSON.stringify(allImages))
+  }
+
+  deleteFromStorage(chosenPhotoIndex) {
+    allImages.splice(chosenPhotoIndex, 1)
+    this.saveToStorage();
+  }
 }
--- main.js
+++ main.js
@@ -7,17 +7,17 @@ const choosePhoto = document.querySelector('.choose-file');
 const viewFavorites = document.querySelector('.view-faves-button');
 const addPhotos = document.querySelector('.add-button');
 const photoGallery = document.querySelector('.display-photos')
-let allImages = [];
+let allImages = JSON.parse(localStorage.getItem('imagesLocalStorage')) || [];
 
 // EVENT LISTENERS
 
 window.addEventListener('load', displayPhotos);
 addPhotos.addEventListener('click', selectPhotos);
+photoGallery.addEventListener('click', managePost)
 
 function displayPhotos() {
-  console.log('display')
   allImages.forEach((photo) => {
-    return populatePhoto(photo.title, photo.caption, photo.file, photo.id, photo.favorite)
+    populatePhoto(photo.title, photo.caption, photo.file, photo.id, photo.favorite)
   })
   allImages.forEach((image) => {
     return new Photo(image.title, image.caption, image.favorite, image.id, image.favorite)
@@ -25,7 +25,6 @@ function displayPhotos() {
 }
 
 function selectPhotos(e) {
-  console.log('select')
   e.preventDefault();
   if(choosePhoto.files[0]){
     reader.readAsDataURL(choosePhoto.files[0]); 
@@ -34,23 +33,67 @@ function selectPhotos(e) {
 }
 
 function addNewPhoto(e) {
-  console.log('add')
   e.preventDefault();
   const newPhoto = new Photo(title.value, caption.value, e.target.result, Date.now())
-  allImages.push(newPhoto);
   populatePhoto(newPhoto.title, newPhoto.caption, newPhoto.file, newPhoto.id)
+  allImages.push(newPhoto);
+  newPhoto.saveToStorage(allImages)
 }
 
 function populatePhoto(title, caption, file, id, favorite) {
-  console.log('populate')
+  if (favorite === true) {
+    var favoriteSVG = `"assets/favorite-active.svg"`;
+  } else {
+    var favoriteSVG = `"assets/favorite.svg"`;
+  }
   photoGallery.innerHTML +=
    `
    <section class="unique-post" data-id="${id}">
-    <input class="post-title post-text" type="text" value="${title}">
-    <section class="post-image"><img src=${file} /></section>
-    <input class="post-caption post-text" type="text" value="${caption}">
-  </section>
-  <section>
+    <p contenteditable="true" class="post-title post-text" type="text" value="${title}"></p>
+    <section class="post-image-container">
+      <img class="post-image" src=${file} />
+    </section>
+    <p contenteditable="true" class="post-caption post-text" type="text" value="${caption}"></p>
+    <section class="photo-interactive-container">
+      <img class="trash-button" src="assets/delete.svg">
+      <img class="heart-button" src=${favoriteSVG}>
+    </section>
   </section>
   `
+}
+
+function managePost(e) {
+  if(e.target.classList.contains('heart-button')) {
+    makeFavorite(e)
+  }
+  if(e.target.classList.contains('trash-button')) {
+    deletePost(e)
+  }
+}
+
+function makeFavorite(e) {
+  const button = e.target
+  const chosenPhoto = e.target.closest('.unique-post')
+  const chosenPhotoId = parseInt(chosenPhoto.dataset.id)
+  const chosenPhotoIndex = allImages.findIndex((image) => {
+    return image.id === chosenPhotoId
+  })
+  const selected = allImages[chosenPhotoIndex];
+  selected.favorite = !selected.favorite
+  selected.saveToStorage();
+  if(selected.favorite) {
+    button.setAttribute("src", "assets/favorite-active.svg");
+  } else {
+    button.setAttribute("src", "assets/favorite.svg");
+  }
+}
+
+function deletePost(e) {
+  const chosenPhoto = e.target.closest('.unique-post')
+  const chosenPhotoId = parseInt(chosenPhoto.dataset.id)
+  const chosenPhotoIndex = allImages.findIndex((image) => {
+    return image.id === chosenPhotoId
+  })
+  allImages[chosenPhotoIndex].deleteFromStorage(e)
+  chosenPhoto.remove();
 }
